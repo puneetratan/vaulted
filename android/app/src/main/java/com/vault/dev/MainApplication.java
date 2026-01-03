23331430a6bf4ceb79c44e600cddfc991967d65a
puneetratan <puneetsiet@gmail.com>
2026-01-03 14:00:32 -0500

    working on barcode scanner

--- android/app/src/main/java/com/vault/dev/MainApplication.java
+++ android/app/src/main/java/com/vault/dev/MainApplication.java
@@ -53,10 +53,10 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
-    }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    // if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+    //   // If you opted-in for the New Architecture, we load the native entry point for this app.
+    //   DefaultNewArchitectureEntryPoint.load();
+    // }
+    // ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
